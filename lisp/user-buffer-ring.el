345c1baf456ef01ae40bfd60894cb486b83ff0a7
Daniel Hamilton Smith Santa Cruz <daniel.smith@PW0M136227.local>
2026-05-22 08:32:41 -0500

    minor cleanup

--- lisp/user-buffer-ring.el
+++ lisp/user-buffer-ring.el
@@ -36,105 +36,50 @@
 (defun prev-user-buffer-ring ()
   "User-buffer-ring-index + 1."
   (interactive)
-  (if (> (- (length user-buffer-ring) 1) user-buffer-ring-index)
-      (progn
-        (setq user-buffer-ring-index (+ user-buffer-ring-index 1))
-        (get-user-buffer-ring-i)
-        )
-    )
-  )
+  (when (> (- (length user-buffer-ring) 1) user-buffer-ring-index)
+    (setq user-buffer-ring-index (+ user-buffer-ring-index 1))
+    (get-user-buffer-ring-i)))
 
 (defun next-user-buffer-ring ()
   "User-buffer-ring-index - 1."
   (interactive)
-  (if (< 0 user-buffer-ring-index)
-      (progn
-        (setq user-buffer-ring-index (- user-buffer-ring-index 1))
-        (get-user-buffer-ring-i)
-        )
-    )
-  )
+  (when (< 0 user-buffer-ring-index)
+    (setq user-buffer-ring-index (- user-buffer-ring-index 1))
+    (get-user-buffer-ring-i)))
 
 (defun save-to-user-buffer-ring ()
   "Agregar un nuevo buffer al user-buffer-ring."
-  (if (buffer-file-name)
-      (let ((bfn (buffer-name)))
-        (setq user-buffer-ring-index 0)
-        (message (concat "agregando " bfn))
-        (if (member bfn user-buffer-ring)
-            (remove-value-from-user-buffer-ring bfn)
-          )
-        (push bfn user-buffer-ring)
-        )
-    )
-  (if (> (length user-buffer-ring) user-buffer-ring-max)
-      (setq user-buffer-ring (butlast user-buffer-ring))
-      )
-  )
-
-;; (defun user-buffer-ring-contains (bff)
-;;   (if (member bff user-buffer-ring)
-;;       (concat "contiene " bff)
-;;     (concat "no contiene " bff)
-;;     )
-;;   )
+  (when (buffer-file-name)
+    (let ((bfn (buffer-name)))
+      (setq user-buffer-ring-index 0)
+      (message (concat "agregando " bfn))
+      (when (member bfn user-buffer-ring)
+        (remove-value-from-user-buffer-ring bfn))
+      (push bfn user-buffer-ring)))
+  (when (> (length user-buffer-ring) user-buffer-ring-max)
+    (setq user-buffer-ring (butlast user-buffer-ring))))
 
 (defun remove-value-from-user-buffer-ring (bfn)
   "Eliminar el valor BFN del user-buffer-ring."
   (interactive)
-  ;; TODO: podria ser bueno verificar la posicion actual para moverla
-  (setq user-buffer-ring (delete bfn user-buffer-ring))
-  )
+  (setq user-buffer-ring (delete bfn user-buffer-ring)))
 
 (defun remove-from-user-buffer-ring ()
   "Eliminar los buffers del user-buffer-ring."
-  (if (buffer-file-name)
-      (let ((bfn (buffer-name)))
-        (progn
-          (remove-value-from-user-buffer-ring bfn)
-          )
-        )
-    )
-  )
+  (when (buffer-file-name)
+    (remove-value-from-user-buffer-ring (buffer-name))))
 
-;;TODO: poner para que se vuelva a activar esto cuando uno salta a buffers ya abiertos
 (add-hook 'find-file-hook 'save-to-user-buffer-ring)
-;; (add-hook 'buffer-list-update-hook 'save-to-user-buffer-ring)
-
 (add-hook 'kill-buffer-hook 'remove-from-user-buffer-ring)
-
-;; (defun esto-es-una-prueba (a)
-;;   (if (member (buffer-name) user-buffer-ring)
-;;       (progn
-;;         (message "%s" a)
-;;         (message "%s" (buffer-name))
-;;         )
-;;       )
-;;   )
-;;
-;; (push 'esto-es-una-prueba window-buffer-change-functions)
-
-;; (add-hook 'window-buffer-change-functions 'save-to-user-buffer-ring)
 
 
 (defun user-buffer-ring-get-index ()
+  "Update `user-buffer-ring-index` to match the current buffer's position."
   (interactive)
-   (if (member (buffer-name) user-buffer-ring)
-       ;; (message a)
-       (let ((pos (seq-position (buffer-name) user-buffer-ring)))
-         (message (concat "index" user-buffer-ring-index))
-         (message pos)
-         ;; (if pos
-         ;;     (setq user-buffer-ring-index pos)
-         ;;     )
-
-         )
-       )
-   )
-
-;; (add-hook 'window-buffer-change-functions 'user-buffer-ring-get-index)
-
-;; buffer-list-update-hook
+  (let ((pos (seq-position user-buffer-ring (buffer-name))))
+    (when pos
+      (setq user-buffer-ring-index pos)
+      (message "index: %d" user-buffer-ring-index))))
 
 (provide 'user-buffer-ring)
 ;;; user-buffer-ring ends here
